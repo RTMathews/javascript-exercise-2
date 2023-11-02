6597c7b6bb9eeed4724ab5e6834acc9f076aedf5
Ryan <mathewsthomasryan@gmail.com>
2023-11-02 06:16:20 -0500

    blue.style.cssText

--- js-file.js
+++ js-file.js
@@ -10,5 +10,6 @@ container.appendChild(red);
 const blue = document.createElement('h3');
 blue.classList.add('blue');
 blue.textContent = "I'm a blue h3!"
+blue.style.cssText = "color:blue;"
 
 container.appendChild(blue);
