229c24cfb76628c6ff1d2c645d263446c17f241b
Ryan <mathewsthomasryan@gmail.com>
2023-11-02 06:12:34 -0500

    red.style.cssText color:red

--- js-file.js
+++ js-file.js
@@ -3,5 +3,6 @@ const container = document.querySelector('#container');
 const red = document.createElement('p');
 red.classList.add('red');
 red.textContent = "Hey I'm Red!"
+red.style.cssText = "color:red;"
 
 container.appendChild(red);
